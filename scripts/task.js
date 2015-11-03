6580e0eae6046773386ec4de84fd1697c90b916a
Nona MacDermid <nonamacd@gmail.com>
2015-11-02 22:59:22 -0400

    Post works but Title Refresh Not Working

---
+++ scripts/task.js
@@ -0,0 +1,11 @@
+// 'use strict';
+
+// app.controller('TaskController', function($scope) {
+// 	$scope.title = "Cleaning my house";
+// });
+
+'use strict';
+
+app.controller('TaskController', ['$scope', function($scope) {
+	$scope.title = "Cleaning my house";
+}]);
